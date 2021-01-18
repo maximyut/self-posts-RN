8d7d9bfa45ed3d8deb7d60adde333f477686e6c0
maximyut <garfonze1@gmail.com>
2021-01-18 12:59:19 +0300

    bottom navigation for android

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,8 +1,12 @@
+import React from "react";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import { createAppContainer } from "react-navigation";
 import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "react-navigation-stack";
 import { MainScreen } from "../screens/MainScreen";
 import { PostScreen } from "../screens/PostScreen";
+import { BookedScreen } from "../screens/BookedScreen";
 import { THEME } from "../theme";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
@@ -24,4 +28,55 @@ const PostNavigator = createStackNavigator(
   }
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookedScreen,
+    Post: PostScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "white",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : THEME.MAIN_COLOR,
+    },
+  }
+);
+
+const bottomTabsConfig = {
+  Post: {
+    screen: PostNavigator,
+    navigationOptions: {
+      tabBarLabel: "Все",
+      tabBarIcon: (info) => (
+        <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+      ),
+    },
+  },
+  Booked: {
+    screen: BookedNavigator,
+    navigationOptions: {
+      tabBarLabel: "Избранное",
+      tabBarIcon: (info) => (
+        <Ionicons name="ios-star" size={25} color={info.tintColor} />
+      ),
+    },
+  },
+};
+
+const BottomNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+      activeTintColor: "white",
+      shifting: true,
+        barStyle: {
+          backgroundColor: THEME.MAIN_COLOR,
+        },
+      })
+    : createBottomTabNavigator(bottomTabsConfig, {
+        tabBarOptions: {
+          activeTintColor: "THEME.MAIN_COLOR",
+        },
+      });
+
+export const AppNavigation = createAppContainer(BottomNavigator);
--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,12 +1,58 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  StatusBar,
+} from "react-native";
+import { Post } from "../components/Post";
+import { DATA } from "../data";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-export const BookedScreen = ({}) => {
+export const BookedScreen = ({ navigation }) => {
+  const openpostHandler = (post) => {
+    navigation.navigate("Post", {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked,
+    });
+  };
+
   return (
-    <View style={styles.center}>
-      <Text>BookedScreen</Text>
+    <View style={styles.wrapper}>
+      <StatusBar backgroundColor="black" barStyle="default" />
+      <FlatList
+        data={DATA.filter(post => post.booked)}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({ item }) => <Post post={item} onOpen={openpostHandler} />}
+      />
     </View>
   );
+};
+
+BookedScreen.navigationOptions = {
+  headerTitle: "Избранное",
+  headerRight: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Take photo"
+        iconName="ios-camera"
+        onPress={() => console.log("press photo")}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Toggle Drawer"
+        iconName="ios-menu"
+        onPress={() => console.log("press photo")}
+      />
+    </HeaderButtons>
+  ),
 };
 
 const styles = StyleSheet.create({
@@ -14,5 +60,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  wrapper: {
+    padding: 10,
   },
 });
